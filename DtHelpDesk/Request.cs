a9f931163b120576bb485d17b2ee57dbc392524b
Oleg Borkhalenko <borkhalenko@gmail.com>
2018-04-17 15:08:09 +0300

    Added new project DtKpp

---
+++ DtHelpDesk/Request.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtItHelpDesk {
+    class Request {
+        private TradePoint point;
+        private DateTime date;
+        private String description;
+
+        internal TradePoint Point {
+            get {
+                return point;
+            }
+
+            set {
+                point = value;
+            }
+        }
+
+        public DateTime Date {
+            get {
+                return date;
+            }
+
+            set {
+                date = value;
+            }
+        }
+
+        public string Description {
+            get {
+                return description;
+            }
+
+            set {
+                description = value;
+            }
+        }
+    }
+}
---
+++ DtHelpDesk/TradePoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtItHelpDesk {
+    class TradePoint {
+        private string namePoint;
+        private string address;
+
+        public string NamePoint {
+            get {
+                return namePoint;
+            }
+
+            set {
+                namePoint = value;
+            }
+        }
+
+        public string Address {
+            get {
+                return address;
+            }
+
+            set {
+                address = value;
+            }
+        }
+    }
+}
