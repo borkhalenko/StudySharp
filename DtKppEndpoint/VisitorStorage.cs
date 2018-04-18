fc893b7e2a3539291657bc1cd6681f5b104892f9
Oleg Borkhalenko <borkhalenko@gmail.com>
2018-04-18 17:46:08 +0300

    Modified classes Visitor and VisitorStorage.

---
+++ DtKppEndpoint/VisitorStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtKppEndpoint {
+    public class VisitorStorage {
+        private Dictionary<int, Visitor> visitorStorage;
+        private Dictionary<string, int> codeStorage; 
+
+
+        //public Visitor GetVisitorById(Id id) {
+        //    if (storage.ContainsKey(id)) {
+        //        return storage[id];
+        //    }
+        //    else {
+        //        return null;
+        //    }
+        //} 
+
+        //public bool ContainsCode()
+
+        //public bool ContainsId(Guid id) {
+        //    return storage.ContainsKey(id);
+        //}
+    }
+}
