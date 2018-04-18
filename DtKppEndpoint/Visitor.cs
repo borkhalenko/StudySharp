f427107fd5bfa2258b5733ec58939da6e007d575
Oleg Borkhalenko <borkhalenko@gmail.com>
2018-04-18 17:46:08 +0300

    Modified classes Visitor and VisitorStorage.

---
+++ DtKppEndpoint/Visitor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtKppEndpoint {
+    public class Visitor {
+        private int id;
+        private string name;
+        private List<string> codes = new List<string>();
+
+        public Visitor(int p_id) {
+            Id = p_id;
+        }
+
+        public Visitor(int p_id, string p_name) {
+            Id = p_id;
+            Name = p_name;
+        }
+
+        public void AddCode(string code) {
+            if (!codes.Contains(code)) {
+                codes.Add(code);
+            }
+        }
+
+        public void DeleteCode(string code) {
+            if (codes.Contains(code)) {
+                codes.Remove(code);
+            }
+        }
+
+        public int Id {
+            get {
+                return id;
+            }
+
+            private set {
+                id = value;
+            }
+        }
+
+        public string Name {
+            get {
+                return name;
+            }
+
+            set {
+                name = value;
+            }
+        }
+    }
+}
---
+++ DtKppEndpoint/VisitorStorage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtKppEndpoint {
+    public class VisitorStorage {
+        private Dictionary<int, Visitor> visitorStorage;
+        private Dictionary<string, int> codeStorage;
+
+        public IReader reader;
+
+        public Visitor GetVisitorById(int id) {
+            if (visitorStorage.ContainsKey(id)) {
+                return visitorStorage[id];
+            }
+            else {
+                return null;
+            }
+        }
+
+        public bool ContainsCode(string code) {
+            if (codeStorage.ContainsKey(code)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public bool UpdateStorage() {
+            
+            return true;
+        }
+    }
+}
