fe7bbbab62171f8de02fa9adf7f221a88e07aff9
Oleg Borkhalenko <borkhalenko@gmail.com>
2018-05-21 18:14:11 +0300

    Worked with settings.

--- DtKppEndpoint/Program.cs
+++ DtKppEndpoint/Program.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//https://habr.com/post/271483/
 namespace DtKppEndpoint {
     class Program {
         static void Main(string[] args) {
-            Settings settings = new Settings();
+            Settings settings = Settings.GetInstance();
             Console.WriteLine(settings.GetStorageType());
             //Endpoint e = new Endpoint();
             //e.start();
